cc5d14bd3641b01c6fab5c0260706c8e083b0564
NoahKRAllen <NoahKRAllen@gmail.com>
2025-11-16 13:48:16 -0800

    Swapped to sys.argv for a more robust bookbot through the terminal

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-﻿from stats import num_words_in_string
+﻿import sys
+from stats import num_words_in_string, amount_of_each_char, sorted_dict
 def get_book_text(book_to_parse):
     with open(book_to_parse, encoding="utf-8") as f:
         return f.read()
+def pretty_print(num_words, dict_of_chars):
+    print("============ BOOKBOT ============"
+          f"\nAnalyzing book found at {sys.argv[1]}..."
+          "\n----------- Word Count ----------"
+          "\nFound " + str(num_words) + " total words"
+          "\n--------- Character Count -------")
+    for char, count in dict_of_chars.items():
+        if char.isalpha():
+            print(char + ": " + str(count))
 def main():
-    num_words = num_words_in_string(get_book_text("books/frankenstein.txt"))
-    print(f"Found {num_words} total words")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    num_words = num_words_in_string(get_book_text(sys.argv[1]))
+    dict_of_chars = amount_of_each_char(get_book_text(sys.argv[1]))
+    sorted_dict_of_chars = sorted_dict(dict_of_chars)
+    pretty_print(num_words, sorted_dict_of_chars)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,2 +1,15 @@
-﻿def num_words_in_string(string):
+﻿from typing import Any
+
+
+def num_words_in_string(string):
     return len(string.split())
+
+def amount_of_each_char(string):
+    lower_string = string.lower()
+    char_dict = {}
+    for char in lower_string:
+        char_dict[char] = char_dict.get(char, 0) + 1
+    return char_dict
+
+def sorted_dict(dict_to_sort):
+    return dict(sorted(dict_to_sort.items(), key=lambda item: item[1], reverse=True))
